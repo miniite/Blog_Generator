766e4c15d077a4955e2cf364e9b3e2710803b2db
Milen Ninan Ittyeipe <ninanittyeipe@gmail.com>
2025-05-26 21:02:07 +0530

    Deployment_Updated_4

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain_huggingface import HuggingFaceEndpoint
 # Load .env file for HuggingFace Token
 load_dotenv()
 st.secrets = 'HF_TOKEN'
-os.environ['HF_TOKEN']=st.secrets.get("HF_TOKEN")
+os.environ['HF_TOKEN']=st.secrets["HF_TOKEN"]
 
 
 ## Function To get response from Model
